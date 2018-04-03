8685e85b52e2da4f4f19e04f7334d88504701c2f
Alex <a.j.newb@gmail.com>
2018-04-03 17:45:21 +0100

    Write property test - it reveals a rounding problem (still failing)

--- build.sbt
+++ build.sbt
@@ -4,3 +4,4 @@ name := "capgemini-remote-technical-exercise"
 scalaVersion := "2.12.4"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.4" % "test"
--- src/test/scala/CheckoutTest.scala
+++ src/test/scala/CheckoutTest.scala
@@ -1,6 +1,8 @@
+import org.scalacheck.Gen
 import org.scalatest.FlatSpec
+import org.scalatest.prop.PropertyChecks
 
-final class CheckoutTest extends FlatSpec {
+final class CheckoutTest extends FlatSpec with PropertyChecks {
   "The checkout" should "return 0.0 for an empty input list" in {
     val expectedOutput = 0.0
     val testOutput = Checkout.checkout(List.empty)
@@ -23,5 +25,14 @@ final class CheckoutTest extends FlatSpec {
     val expectedOutput = 0.85
     val testOutput = Checkout.checkout(List[Fruit](Apple, Orange))
     assert(testOutput == expectedOutput)
+  }
+
+  it should "return the sum of the apple prices and orange prices for an arbitrary list" in {
+    forAll(Gen.listOf(Gen.oneOf(Apple, Orange))) { fruitList =>
+      val expectedOutput = (0.6 * fruitList.count(_ == Apple)) + (0.25 * fruitList.count(_ == Orange))
+      val testOutput = Checkout.checkout(fruitList)
+      println(testOutput)
+      assert(testOutput == expectedOutput)
+    }
   }
 }
