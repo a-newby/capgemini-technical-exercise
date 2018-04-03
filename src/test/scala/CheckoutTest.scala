345feeac07b6af5f3752a9941c163c0c298d5894
Alex <a.j.newb@gmail.com>
2018-04-03 17:32:47 +0100

    Add failing test for list with an Apple

--- src/test/scala/CheckoutTest.scala
+++ src/test/scala/CheckoutTest.scala
@@ -6,4 +6,10 @@ final class CheckoutTest extends FlatSpec {
     val testOutput = Checkout.checkout(List.empty)
     assert(testOutput == expectedOutput)
   }
+
+  it should "return 0.6 for a list with an Apple" in {
+    val expectedOutput = 0.6
+    val testOutput = Checkout.checkout(List[Fruit](Apple))
+    assert(testOutput == expectedOutput)
+  }
 }
