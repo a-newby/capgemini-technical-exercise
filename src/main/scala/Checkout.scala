02d69f4072389e3b25825ce9f1bc145a8f160af5
Alex <a.j.newb@gmail.com>
2018-04-03 17:26:47 +0100

    Add unimplemented function under test. Also added some helpful data types

---
+++ src/main/scala/Checkout.scala
@@ -0,0 +1,4 @@
+
+object Checkout {
+  def checkout(items: List[Fruit]): Int = ??? //Unimplemented - awaiting TDD
+}
---
+++ src/main/scala/Fruit.scala
@@ -0,0 +1,5 @@
+
+sealed trait Fruit
+
+case object Apple extends Fruit
+case object Orange extends Fruit
