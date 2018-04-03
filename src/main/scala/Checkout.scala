71d151bde68ee401a8667b0913b84c4208964fb8
Alex <a.j.newb@gmail.com>
2018-04-03 18:03:38 +0100

    Make Checkout apply the three-for-the-price-of-two deal on Oranges

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -3,6 +3,7 @@ object Checkout {
   def checkout(items: List[Fruit]): BigDecimal = {
     val basePrice = items.map(_.price).sum
     val appleDiscount = BigDecimal(0.6) * (items.count(_ == Apple) / 2)
-    basePrice - appleDiscount
+    val orangeDiscout = BigDecimal(0.25) * (items.count(_ == Orange) / 3)
+    (basePrice - appleDiscount) - orangeDiscout
   }
 }
